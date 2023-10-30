62817fb1667c005b6df7bc1178728fbdc1a68da8
Shahzad Ali <shahzad572a@gmail.com>
2023-10-30 15:01:22 +0500

    .

--- server/controlar/auth.js
+++ server/controlar/auth.js
@@ -1,28 +1,28 @@
 import express from "express";
 import UserModel from "../model/user.js";
 
-export const signUp = async (req,res) => {
- 
-      const { name, email, password } = req.body
+export const signUp = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await UserModel.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ error: 'User already exists' });
+  }
+
+  try {
+    const user = new UserModel({
+      name,
+      email,
+      password
+    });
     
-      const userExists = await UserModel.findOne({ email })
-      if (userExists) {
-        res.status(400)
-        throw new Error('User already exists')
-      }
-      const user = await UserModel.create({
-        name,
-        email,
-        password,
-      })
-      if (user) {
-        res.status(201).json({
-          success: true
-        })
-      } else {
-        res.status(400)
-        throw new Error('Invalid user data')
-      }
-    }
+
+    await user.save();
+    res.status(201).json({ success: true });
+  } catch (error) {
+    console.error('User save error:', error);
+    res.status(400).json({ error: 'Failed to save user data' });
+  }
+};
 
     
