ee5107eae0f2fac551a43b471ee274dbca072f2a
Shahzad Ali <shahzad572a@gmail.com>
2023-10-31 13:32:45 +0500

    ..

--- server/controlar/auth.js
+++ server/controlar/auth.js
@@ -1,28 +1,30 @@
 import express from "express";
 import UserModel from "../model/user.js";
+import bcryptjs from 'bcryptjs'
+import asyncHandler from 'express-async-handler'
 
-export const signUp = async (req, res) => {
+
+export const signUp = asyncHandler (async (req, res) => {
   const { name, email, password } = req.body;
 
-  const userExists = await UserModel.findOne({ email });
-  if (userExists) {
-    return res.status(400).json({ error: 'User already exists' });
-  }
-
   try {
+    const userExists = await UserModel.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
     const user = new UserModel({
       name,
       email,
-      password
+      hashed_password: hashedPassword // Store the hashed password
     });
-    
 
     await user.save();
     res.status(201).json({ success: true });
   } catch (error) {
     console.error('User save error:', error);
-    res.status(400).json({ error: 'Failed to save user data' });
+    res.status(500).json({ error: 'Failed to save user data' });
   }
-};
-
-    
+});
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,18 +1,9 @@
 import { check } from "express-validator";
 
 export const userSignupValidator = [
-    check('name')
-        .not()
-        .isEmpty()
-        .withMessage('Name is required'),
-
-    check('email')
-        .not()
-        .isEmpty()
-        .isEmail()
-        .withMessage('Valid email is required'),
-
-    check('password')
-        .isLength({ min: 6 })
-        .withMessage('Password must be at least 6 characters long')
+    check("name").not().isEmpty().withMessage("Name is required"),
+    check("email").isEmail().withMessage("Valid email is required"),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
 ];
